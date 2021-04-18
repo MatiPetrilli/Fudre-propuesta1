2ea7c44a183a7a6428a8ce9da92175e01dc246f6
Melina Macko <melina.julieta.macko@gmail.com>
2021-04-18 18:55:15 -0300

    agrego envio de mails

---
+++ sendmail.php
@@ -0,0 +1,33 @@
+<?php
+ ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $to = "membresias@fudre.com.ar";
+    $subject = "Suscripcion a membresia!";
+    $message = "Hola! Quiero registrarme a Fudre!\n
+                Nombre: ". $_POST['Nombre'] . "\n
+                Apellido: " . $_POST['Apellido'] . "\n
+                Mail: " . $_POST['Email'] . "\n
+                Telefono: " . $_POST['Telefono'] . "\n
+                DNI: " . $_POST['DNI'] . "\r\n\r\n
+                --------- DATOS DE ENTREGA -----------\r\n
+                Calle: " . $_POST['Calle'] . "\n
+                Altura: " . $_POST['Altura'] . "\n
+                Piso / puerta: " . $_POST['Piso'] . "\n
+                Codigo postal: " . $_POST['Codigo-Postal'] . "\n
+                Ciudad: " . $_POST['Ciudad'] . "\n
+                Provincia: " . $_POST['Provincia'] . "\r\n\r\n
+                --------- DATOS DEL PRODUCTO ----------\r\n
+                Membresia: " . $_POST['Membresia'] . "\n
+                Botellas: " . $_POST['Botellas'] . "\n
+                Comentarios: " . $_POST['Informacion'] . "\n";
+    
+    if(mail($to,$subject,$message)) {
+		header("Location: https://www.mercadopago.com/mla/debits/new?preapproval_plan_id=2c93808477ddcf340177dee284d902ea");
+        die();
+        //echo "The email message was sent.";
+    } else {
+        //echo "The email message NOT was sent.";
+    	header("Location: index.html");
+        die();
+    }
+?>
